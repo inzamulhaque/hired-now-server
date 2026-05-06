3ca0d1dba10da6068f24683fffdbcf254c18ce6d
inzamulhaque <mdihalif@gmail.com>
2026-05-06 20:27:13 +0600

    setup prisma

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,6 +6,11 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 const config = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
+
+  prisma: {
+    db_url: process.env.DATABASE_URL,
+    direct_url: process.env.DIRECT_URL,
+  },
 };
 
 export default config;
---
+++ src/shared/prisma.ts
@@ -0,0 +1,11 @@
+import config from "../config/index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client.js";
+
+const adapter = new PrismaPg({ connectionString: config.prisma.db_url });
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export default prisma;
