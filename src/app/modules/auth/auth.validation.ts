a8f56ff95bc65c826f38fcee7d7687cca2b7462c
inzamulhaque <mdihalif@gmail.com>
2026-05-07 19:23:58 +0600

    create zod validation schema for create new employer

---
+++ src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { AccountStatus, Role } from "../../../generated/enums.js";
+
+export const createUserValidationSchema = z.object({
+  email: z.templateLiteral([z.string().min(1), "@", z.string().max(64)]),
+
+  name: z
+    .string()
+    .trim()
+    .min(2, {
+      error: "Name must be at least 2 characters long",
+    })
+    .max(50, {
+      error: "Name cannot exceed 50 characters",
+    }),
+
+  password: z
+    .string()
+    .min(6, {
+      error: "Password must be at least 6 characters long",
+    })
+    .max(100, {
+      error: "Password cannot exceed 100 characters",
+    })
+    .regex(/[A-Z]/, {
+      error: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[a-z]/, {
+      error: "Password must contain at least one lowercase letter",
+    })
+    .regex(/[0-9]/, {
+      error: "Password must contain at least one number",
+    })
+    .regex(/[^A-Za-z0-9]/, {
+      error: "Password must contain at least one special character",
+    }),
+
+  role: z.enum(Role).optional().default(Role.EMPLOYER),
+
+  status: z
+    .enum(AccountStatus)
+    .optional()
+    .default(AccountStatus.PENDING_VERIFICATION),
+});
